539347f14a22c1a34118baf80736673a95253141
Rushalini <rushaliniammu@gmail.com>
2026-02-03 14:30:39 +0530

    Backend

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const todoRoute= require("./router/todoRouter")
+const dotenv=require("dotenv");
+const userRoute = require("./router/userRouter");
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use("/api/user",userRoute);
+app.use("/api/todo",todoRoute);
+const PORT=process.env.PORT || 3000;
+// app.get("/",(req,res)=>{
+//     res.send("Get Route is Working")
+// })
+// app.post("/create",(req,res)=>{
+//     res.json({
+//         message: "Post Route is Working"
+//     })
+// })
+// app.put("/",(req,res)=>{
+//     res.status(200).json({
+//         message: "Put Route is Working"
+//     })
+
+// })
+// app.delete("/",(req,res)=>{
+//     res.status(200).json({
+//         message: "Delete Route is Working"
+//     })
+// })
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
